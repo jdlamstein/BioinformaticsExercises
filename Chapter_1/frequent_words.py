469a411e61f3d34f5bbd3140b46c94bbf2e70774
Joshua Lamstein <jdlamstein@gmail.com>
2025-10-16 20:00:36 -0700

    got boxes in ecoli

--- Chapter_1/frequent_words.py
+++ Chapter_1/frequent_words.py
@@ -75,26 +75,36 @@ def find_clumps(f, k=None, ell=None, t=None):
         freq_map = {}
 
         for i in tqdm(range(n - ell)):
-            if i > 0:
-                window[1:] + txt[i + ell - 1]
-                assert len(window) == ell
-
-                n = len(txt)
-
+            if i == 0:
+                # Calculate the window
                 for j in range(ell - k):
-                    pttern = txt[j : j + k]
+                    pttern = window[j : j + k]
                     if pttern not in freq_map:
                         freq_map[pttern] = 1
                     else:
                         freq_map[pttern] += 1
+                for s in freq_map.keys():
+                    if freq_map[s] >= t:
+                        patterns.add(s)
+            else:
+                new_mer = window[-k + 1 :] + txt[i + ell - 1]
+                old_mer = window[:k]
+                window = txt[i : ell + i]
+                if old_mer in freq_map:
+                    freq_map[old_mer] -= 1
+                    if freq_map[old_mer] < 0:
+                        freq_map[old_mer] = 0
+                if new_mer not in freq_map:
+                    freq_map[new_mer] = 1
+                else:
+                    freq_map[new_mer] += 1
 
-            for s in freq_map.keys():
-                if freq_map[s] >= t:
-                    patterns.add(s)
+                if freq_map[new_mer] >= t:
+                    patterns.add(new_mer)
     res = list(patterns)
     print(f"Count: {len(res)}")
     result = " ".join(res)
-    print(f"answer: {result}")
+    # print(f"answer: {result}")
     return result
 
 
